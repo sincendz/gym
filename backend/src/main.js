0ad80de304df0b45707ceccdeca4fde55350b0b1
Mario Aragao <marioestudos.nuvem@gmail.com>
2025-08-06 22:42:32 -0300

    remove comment

--- backend/src/main.js
+++ backend/src/main.js
@@ -11,7 +11,6 @@ app.get("/", (req, res) => {
   res.send("Olá,mundo!!");
 });
 
-// Sincroniza o banco e só depois inicia o servidor
 const startServer = async () => {
   try {
     await sequelize.sync();
